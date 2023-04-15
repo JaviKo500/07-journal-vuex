4e9d615c108495f292c5ac93568cf41c6ae0a7f0
JaViKo <javikogutierrez64@gmail.com>
2023-04-15 10:30:52 -0500

    entry list and entry design

--- src/modules/dayBook/components/EntryListComponent.vue
+++ src/modules/dayBook/components/EntryListComponent.vue
@@ -1,13 +1,34 @@
 <template>
-  <h1>Entry List</h1>
+  <div class="entry-list-container">
+    <div class="px-2 pt-2">
+      <input 
+        type="text"
+        class="form-control"
+        placeholder="Search Entry">
+    </div>
+
+    <div class="entry-scroll-area">
+        <Entry  v-for="item in 100" :key="item"/>
+    </div>
+  </div>
 </template>
 
 <script>
+import { defineAsyncComponent } from 'vue'
 export default {
-
+  components: {
+    Entry: defineAsyncComponent( () => import('./EntryComponent'))
+  }
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.entry-list-container {
+  border-right: 1px solid #2c3e50;
+  height: calc( 100vh - 56px );
+}
+.entry-scroll-area {
+  height:  calc( 100vh - 110px );
+  overflow: scroll;
+}
 </style>
