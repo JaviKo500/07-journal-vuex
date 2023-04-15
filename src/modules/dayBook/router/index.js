9546cd35c869a1f4e2daf6e686f175f178f681d4
JaViKo <javikogutierrez64@gmail.com>
2023-04-15 10:38:00 -0500

    no entry selected

--- src/modules/dayBook/router/index.js
+++ src/modules/dayBook/router/index.js
@@ -2,6 +2,10 @@ export default {
     name: 'daybook',
     component: () => import(/* webpackChunkName: "DayBook" */ '@/modules/dayBook/layouts/DayBookLayout'),
     children: [
-        
+        {
+            path: '',
+            name: 'no-entry',
+            component: () => import(/* webpackChunkName: "NoEntry" */ '@/modules/dayBook/views/NoEntrySelected'),
+        },
     ]
 }
