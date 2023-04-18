42b31375ed07f120edf48c5d6c95b87bd564e331
JaViKo <javikogutierrez64@gmail.com>
2023-04-17 23:12:25 -0500

    add items in array

--- src/modules/dayBook/store/journal/state.js
+++ src/modules/dayBook/store/journal/state.js
@@ -1,4 +1,23 @@
 export default () => ({
     isLoading: true,
-    entries: [1,2,3,4]
+    entries: [
+        {
+            id: new Date().getTime(),
+            date: new Date().toDateString(),
+            text: 'Ullamco duis enim labore eu amet id irure laboris minim. Est labore veniam enim laboris mollit ea consectetur in eu elit ex reprehenderit adipisicing. Proident quis veniam irure laborum incididunt enim in nostrud eu anim ullamco duis. Irure consequat est ut commodo velit veniam.',
+            picture: null
+        },
+        {
+            id: new Date().getTime() + 1000,
+            date: new Date().toDateString(),
+            text: 'Est labore veniam enim laboris mollit ea consectetur in eu elit ex reprehenderit adipisicing. Proident quis veniam irure laborum incididunt enim in nostrud eu anim ullamco duis. Irure consequat est ut commodo velit veniam.',
+            picture: null
+        },
+        {
+            id: new Date().getTime() + 2000,
+            date: new Date().toDateString(),
+            text: 'Consectetur in eu elit ex reprehenderit adipisicing. Proident quis veniam irure laborum incididunt enim in nostrud eu anim ullamco duis. Irure consequat est ut commodo velit veniam.',
+            picture: null
+        },
+    ]
 })
