b0c3d429434aa1731e783e3c84033e4046bf3f52
JaViKo <javikogutierrez64@gmail.com>
2023-04-27 23:08:35 -0500

    add props id in route

--- src/modules/dayBook/router/index.js
+++ src/modules/dayBook/router/index.js
@@ -11,6 +11,11 @@ export default {
             path: ':id',
             name: 'entry',
             component: () => import(/* webpackChunkName: "EntryView" */ '@/modules/dayBook/views/EntryView'),
+            props: ( route ) => {
+                return {
+                    id: route.params.id
+                }
+            }
         },
     ]
 }
