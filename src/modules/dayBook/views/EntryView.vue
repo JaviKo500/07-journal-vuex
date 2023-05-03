d53d9284b9bdf692da95fe2cb6342672a6b96c85
JaViKo <javikogutierrez64@gmail.com>
2023-05-03 05:40:58 -0500

    load entry when change url id

--- src/modules/dayBook/views/EntryView.vue
+++ src/modules/dayBook/views/EntryView.vue
@@ -1,29 +1,31 @@
 <template>
-  <div class="entry-title d-flex justify-content-between p-2">
-    <div>
-        <span class="text-success fs-3 fw-bold">{{ day }}</span>
-        <span class="mx-1 fs-3">{{ month }}</span>
-        <span class="mx-2 fs-3 fw-light">{{ year }}</span>
-    </div>
-    <div class="">
-        <button class="btn btn-danger mx-2">
-            Delete 
-            <i class="fa fa-trash-alt"></i>
-        </button>
-        <button class="btn btn-primary mx-2">
-            image
-            <i class="fa fa-upload"></i>
-        </button>
-    </div>
-  </div>
-  <hr>
-  <div class="d-flex flex-column px-3 h-75">
-    <textarea 
-        name="" 
-        id="" 
-        cols="30" rows="10" placeholder="what happened today?"
-        v-model="entry.text"></textarea>
-  </div>
+    <template v-if="entry">
+        <div class="entry-title d-flex justify-content-between p-2">
+            <div>
+                <span class="text-success fs-3 fw-bold">{{ day }}</span>
+                <span class="mx-1 fs-3">{{ month }}</span>
+                <span class="mx-2 fs-3 fw-light">{{ year }}</span>
+            </div>
+            <div class="">
+                <button class="btn btn-danger mx-2">
+                    Delete 
+                    <i class="fa fa-trash-alt"></i>
+                </button>
+                <button class="btn btn-primary mx-2">
+                    image
+                    <i class="fa fa-upload"></i>
+                </button>
+            </div>
+        </div>
+        <hr>
+        <div class="d-flex flex-column px-3 h-75">
+            <textarea 
+                name="" 
+                id="" 
+                cols="30" rows="10" placeholder="what happened today?"
+                v-model="entry.text"></textarea>
+        </div>
+    </template>
   <Fab :icon="'fa-floppy-disk'" />
   <img 
     class="img-thumbnail"
@@ -68,13 +70,18 @@ export default {
     methods: {
         loadEntry() {
             const entry = this.getEntryById( this.id )
-            if( !entry ) this.$router.push({ name: 'no-entry' })
-            this.entry = entry;
+            if( !entry ) return this.$router.push({ name: 'no-entry' })
+            this.entry = entry
             console.log(entry)
         }
     },
     created(){
         this.loadEntry();
+    },
+    watch: {
+        id() {
+            this.loadEntry()
+        }
     }
 
 }
