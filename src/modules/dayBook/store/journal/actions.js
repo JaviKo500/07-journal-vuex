7d4262cfc6777740ad0bad1ddebd0359602865c0
JaViKo <javikogutierrez64@gmail.com>
2023-05-04 05:44:51 -0500

    commit new entries

--- src/modules/dayBook/store/journal/actions.js
+++ src/modules/dayBook/store/journal/actions.js
@@ -1,8 +1,15 @@
 import journalApi from "@/api/journalApi"
 
-export const loadEntries = async( /*{ commit }*/ ) => {
+export const loadEntries = async( { commit } ) => {
     const {data} = await journalApi.get( '/entries.json' )
-    console.log(data);
+    const entries = [];
+    for (const id of Object.keys( data )) {
+        entries.push({
+            id, 
+            ...data[id]
+        })
+    }
+    commit('setEntries', entries);
 }
 
 export const updateEntry = async( /*{ commit }*/ ) => {
--- src/modules/dayBook/store/journal/state.js
+++ src/modules/dayBook/store/journal/state.js
@@ -1,23 +1,4 @@
 export default () => ({
     isLoading: true,
-    entries: [
-        {
-            id: '1',
-            date: new Date().toDateString(),
-            text: 'Ullamco duis enim labore eu amet id irure laboris minim. Est labore veniam enim laboris mollit ea consectetur in eu elit ex reprehenderit adipisicing. Proident quis veniam irure laborum incididunt enim in nostrud eu anim ullamco duis. Irure consequat est ut commodo velit veniam.',
-            picture: null
-        },
-        {
-            id: '2',
-            date: new Date().toDateString(),
-            text: 'Est labore veniam enim laboris mollit ea consectetur in eu elit ex reprehenderit adipisicing. Proident quis veniam irure laborum incididunt enim in nostrud eu anim ullamco duis. Irure consequat est ut commodo velit veniam.',
-            picture: null
-        },
-        {
-            id: '3',
-            date: new Date().toDateString(),
-            text: 'Consectetur in eu elit ex reprehenderit adipisicing. Proident quis veniam irure laborum incididunt enim in nostrud eu anim ullamco duis. Irure consequat est ut commodo velit veniam.',
-            picture: null
-        },
-    ]
+    entries: []
 })
