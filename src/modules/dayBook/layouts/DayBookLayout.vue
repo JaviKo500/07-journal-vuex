129ec1fea2f686358d820968715ba806bc379887
JaViKo <javikogutierrez64@gmail.com>
2023-05-04 05:55:06 -0500

    add loading in home screen

--- src/modules/dayBook/layouts/DayBookLayout.vue
+++ src/modules/dayBook/layouts/DayBookLayout.vue
@@ -1,6 +1,14 @@
 <template>
     <Navbar/>
-    <div class="d-flex" >
+    <div class="row justify-content-md-center" v-if="isLoading">
+        <div class="col-3 alert alert-info text-center mt-5">
+            one moment please...
+            <h3 class="mt-2">
+                <i class="fa fa-spin fa-sync"></i>
+            </h3>
+        </div>
+    </div>
+    <div class="d-flex" v-if="!isLoading">
         <div class="col-4">
             <EntryList/>
         </div>
@@ -11,11 +19,14 @@
 </template>
 <script>
 import { defineAsyncComponent } from 'vue'
-import { mapActions } from 'vuex';
+import { mapActions, mapState } from 'vuex';
 export default {
     components: {
         Navbar: defineAsyncComponent( () => import('../components/NavbarComponent') ),
         EntryList: defineAsyncComponent( () => import('../components/EntryListComponent') )
+    },
+    computed: {
+        ...mapState( 'journal', ['isLoading'])
     },
     methods: {
         ...mapActions( 'journal', ['loadEntries'])
